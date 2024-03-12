2951de7dce2a2b4cb829c3ae63c9302ba3c5c4de
Sebastian Barboza <sbarboza30714@ufide.ac.cr>
2024-03-12 15:18:20 -0600

    Corrección de apartado de 'Reservas'

--- proyectoFinal/src/main/java/proyectoFinal/controller/reservasController.java
+++ proyectoFinal/src/main/java/proyectoFinal/controller/reservasController.java
@@ -1,30 +1,15 @@
 package proyectoFinal.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import proyectoFinal.domain.Reservar;
-import proyectoFinal.service.ReservaService;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/reservas")
 public class reservasController {
 
-    @Autowired
-    private ReservaService reservaService; 
-
-    @GetMapping("/fragmentosReservas")
-    public String mostrarFormularioReservas(Model model) {
-        // Agrega un nuevo objeto reserva al modelo
-        model.addAttribute("reserva", new Reservar());
-        return "index"; // Nombre de la vista
-    }
-
-    @PostMapping("/fragmentosReservas")
-    public String procesarFormularioReservas(@ModelAttribute("reserva") Reservar reserva) {
-        reservaService.guardarReserva(reserva);
-        return "index"; 
+    @GetMapping
+    public String reservas() {
+        return "reservas/fragmentos"; // Nombre de la vista
     }
 }
